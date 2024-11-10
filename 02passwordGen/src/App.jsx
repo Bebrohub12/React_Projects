a84d9c70ef1b63351bbc1389732431fbaa41ca03
Bebrohub12 <sudhirmishra5738@gmail.com>
2024-11-10 13:38:43 +0530

    password generator ui complite

--- 02passwordGen/src/App.jsx
+++ 02passwordGen/src/App.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./index.css"
 
 function App() {
+
+  const [length, setLength] = useState(8);
+  const [charAllow, setCharAllow] = useState(false);
+  const [numAllow,setNumAllow]=useState(false);
+  const [password,setPassword]=useState("")
+
+  // console.log(charAllow,numAllow);
+  
+
+
   return (
-    <div className='bg-red-600'>App</div>
+    <div id='mainDiv'>
+      <div className="child">
+
+        <input type="text" id='inp1' value={password} readOnly />
+        <button id='btn'>copy</button>
+
+        <div>
+          <input type="range" min={6} max={100} onChange={(e) => setLength(e.target.value)} />
+          <label >length:{length}</label>
+
+          <input type="checkbox" value={numAllow} onClick={()=>setNumAllow((prev)=> !prev)} />
+          <label htmlFor="">Number</label>
+
+          <input type="checkbox" value={charAllow} onClick={() => setCharAllow((prev)=>!prev)} />
+          <label htmlFor="">SpacelChar</label>
+        </div>
+      </div>
+
+    </div>
+
+
   )
 }
 
